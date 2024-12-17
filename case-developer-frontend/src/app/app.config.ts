54a4ee46b2b352e3d590a1ea5d2eb0d17d901308
davidveen <veen@pmtgroep.nl>
2024-12-17 18:18:41 +0100

    add http client to app

--- case-developer-frontend/src/app/app.config.ts
+++ case-developer-frontend/src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { deelnemerInterceptor } from './core/interceptors/deelnemer.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([
+        deelnemerInterceptor,
+      ])
+    ),
+  ]
 };
