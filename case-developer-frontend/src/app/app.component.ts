fa65b976fa6707b0e09deedba41bff039d842c41
davidveen <veen@pmtgroep.nl>
2024-12-17 18:23:32 +0100

    implemented app landing

--- case-developer-frontend/src/app/app.component.ts
+++ case-developer-frontend/src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { DeelnemerService } from './core/deelnemer.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VerwachteWaardeService } from './verwachte-waarde.service';
+import { CardComponent } from "./shared/card.component";
+import { PensioenleeftijdKiezerComponent } from "./pensioenleeftijd-kiezer/pensioenleeftijd-kiezer.component";
+import { EurPipe } from '@shared/eur.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, CardComponent, PensioenleeftijdKiezerComponent, EurPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
+  providers: [
+    VerwachteWaardeService
+  ]
 })
-export class AppComponent {
-  title = 'case-developer-frontend';
+export class AppComponent implements OnInit {
+  readonly title = 'case-developer-frontend';
+
+  private readonly deelnemerService = inject(DeelnemerService);
+  private readonly verwachteWaardeService = inject(VerwachteWaardeService);
+
+  protected readonly pensioenleeftijd = { years: 67, months: 3 };
+
+  readonly deelnemer$ = this.deelnemerService.get();
+  readonly verwachteWaarde = this.verwachteWaardeService.verwachteWaarde;
+
+  ngOnInit(): void {
+    this.verwachteWaardeService.update(this.pensioenleeftijd);
+  }
 }
